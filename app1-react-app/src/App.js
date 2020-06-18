e0fee9fe8c182452cd61b53b6ccbfe04cd0a39fc
Muhammad Saqlain Saleem <Miansaqlain52@gmail.com>
2020-06-18 03:20:38 -0700

    update app

--- app1-react-app/src/App.js
+++ app1-react-app/src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Diner from './Diner';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1> Wellcome to Food Menue</h1>
+
+<table width="1300" border="20" bg-color="lighht gray"/>
+ {/* <Diner></Diner> */ }
+ <Diner dishname="Biryani" sweetdish="kheer" />
+ <p>
+ <img src="../images/B.jpg" alt="Biryani" align="middle"/>
+ <br></br>
+ <price> <b>200 PKR/ Only</b></price>
+ <br></br>
+ <a href="https://www.foodpanda.pk/restaurant/u5ma/karachi-biryani-center-phase-3-chowk#"><button>Order Now</button></a>
+
+ 
+  </p>
+
+ <table width="1300" border="20" bg-color="lighht gray"/>
+
+ <Diner dishname="krahi chiken" sweetdish="kulfa" />
+ <p><img src="../images/k.jpg" alt="Chiken krahi" align="middle"/> </p>
+ <p>
+ <price>
+   Price Per serving:<br></br> <b>350 PKR/ Only</b></price>
+ <br></br>
+ <a href="https://www.foodpanda.pk/restaurant/n4yi/chicken-inn-fast-food#"><button>Order Now</button></a>
+   <br/>
+   </p>
+
+ <table width="1300" border="20" bg-color="lighht gray"/>
+
+ <Diner dishname="Nihari" />
+ <p><img src="../images/N.jpg" alt="Nihari" align="middle" width="300px"/> </p>
+ <price>
+   Price Per serving:<br></br> <b>350 PKR/ Only</b></price>
+ <br></br>
+ <a href="https://www.foodpanda.pk/restaurant/n4yi/chicken-inn-fast-food#"><button>Order Now</button></a>
+   <br/>
+
+
+     
     </div>
   );
 }
---
+++ app1-react-app/src/Diner.js
@@ -0,0 +1,18 @@
+import React from'react';
+
+function App(props){
+    return(
+        <div> 
+
+       
+           <b>Today we are serving {props.dishname}</b>
+           <br></br>
+         
+           
+           
+
+        </div>
+
+    )
+}
+   export default App;
